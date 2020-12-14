5d6844fcbc022c1093b938e8d0976b9ce8813d1d
Diana Petre <petre.diana98@yahoo.ro>
2020-12-14 22:17:52 +0200

    created a command collection as an observable collection

---
+++ SimplePaint.ObjectModel/CommandCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimplePaint.ObjectModel
+{
+    public class CommandCollection : ObservableCollectionBase<Command>
+    {
+
+    }
+}
---
+++ SimplePaint.ObjectModel/ObservableCollectionBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace SimplePaint.ObjectModel
+{
+    public class ObservableCollectionBase<T> : ObservableCollection<T>
+        where T : INotifyPropertyChanged
+    {
+        public ObservableCollectionBase()
+        {
+            CollectionChanged += CurrentObservableCollectionChanged;
+        }
+
+        void CurrentObservableCollectionChanged (object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                foreach (T item in e.NewItems)
+                {
+                    item.PropertyChanged += ItemPropertyChanged;
+                }
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                foreach (T item in e.OldItems)
+                {
+                    item.PropertyChanged -= ItemPropertyChanged;
+                }
+            }
+        }
+
+        void ItemPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            NotifyCollectionChangedEventArgs evarg = 
+                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, sender, sender);
+
+            OnCollectionChanged(evarg);
+        }
+    }
+}
