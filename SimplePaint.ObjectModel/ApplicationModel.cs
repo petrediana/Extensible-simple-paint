993fe835dc00a561bd2b484b72afc038a9fa997c
Diana Petre <petre.diana98@yahoo.ro>
2020-12-14 22:26:31 +0200

    created singletone ApplicationModel class to manage only one instance

---
+++ SimplePaint.ObjectModel/ApplicationModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimplePaint.ObjectModel
+{
+    public class ApplicationModel
+    {
+        private ApplicationModel()
+        {
+            _paintDocument = new PaintDocument();
+            _commands = new CommandCollection();
+        }
+        static ApplicationModel()
+        {
+            _instace = new ApplicationModel();
+        }
+
+        public event EventHandler OnQuit;
+
+        #region Private Attributes definition
+        private static readonly ApplicationModel _instace;
+        private PaintDocument _paintDocument;
+        private CommandCollection _commands;
+        #endregion
+
+        #region Public Properties Definitions: Instace, PaintDocument, Commands
+        public static ApplicationModel Instace
+        {
+            get => _instace;
+        }
+
+        public PaintDocument PaintDocument
+        {
+            get => _paintDocument;
+        }
+
+        public CommandCollection Commands
+        {
+            get => _commands;
+        }
+        #endregion
+
+        public void Quit() => OnQuit?.Invoke(this, new EventArgs());
+    }
+}
