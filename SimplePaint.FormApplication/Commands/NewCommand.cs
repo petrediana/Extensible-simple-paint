a08fee0e584265f04870ad7e674d6d83eb597a50
Diana Petre <petre.diana98@yahoo.ro>
2020-12-18 08:50:49 +0200

    added new and quit commands

---
+++ SimplePaint.FormApplication/Commands/NewCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimplePaint.ObjectModel;
+using System.Windows.Forms;
+
+namespace SimplePaint.FormApplication.Commands
+{
+    public class NewCommand : Command
+    {
+        public NewCommand() : base("New") { }
+
+        public override void Execute()
+        {
+            PaintDocument document = ApplicationModel.Instace.PaintDocument;
+            if (document.IsDirty)
+            {
+                DialogResult dialogResult =
+                    MessageBox.Show("Document was modified. You will lose changes. Want to save?",
+                        "Paint!", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+
+                if (dialogResult == DialogResult.No)
+                {
+                    return;
+                }
+            }
+
+            document.New();
+        }
+    }
+}
---
+++ SimplePaint.FormApplication/Commands/QuitCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimplePaint.ObjectModel;
+using System.Windows.Forms;
+
+namespace SimplePaint.FormApplication.Commands
+{
+    public class QuitCommand : Command
+    {
+        public QuitCommand() : base("Quit") { }
+        public override void Execute()
+        {
+            PaintDocument document = ApplicationModel.Instace.PaintDocument;
+            
+            if (document.IsDirty)
+            {
+                DialogResult dialogResult = 
+                    MessageBox.Show("Document was modified. Are you sure you want to quit and lose changes?",
+                        "Quit - Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+
+                if (dialogResult == DialogResult.No)
+                {
+                    return;
+                }
+            }
+
+            ApplicationModel.Instace.Quit();
+        }
+    }
+}
