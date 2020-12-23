02fb6c62e2d94350877cfb7b76bf19689a35a4f2
Diana Petre <petre.diana98@yahoo.ro>
2020-12-23 19:00:49 +0200

    added change color command

---
+++ SimplePaint.FormApplication/Commands/ChangeColorCommand.cs
@@ -0,0 +1,26 @@
+﻿using SimplePaint.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SimplePaint.FormApplication.Commands
+{
+    public class ChangeColorCommand : Command
+    {
+        public ChangeColorCommand() : base("Pick Color") { }
+
+        public override void Execute()
+        {
+            PaintDocument document = ApplicationModel.Instace.PaintDocument;
+            ColorDialog colorDialog = new ColorDialog();
+
+            DialogResult dialogResult = colorDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                document.ChangeColor(colorDialog.Color);
+            }
+        }
+    }
+}
